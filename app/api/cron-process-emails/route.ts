43dda565c4fd71ceb1ced93dfb8cee2ff51371c4
Dev-Iyke <iyke.thompson01@gmail.com>
2025-01-03 04:24:45 +0100

    fixed eslint errors

--- app/api/cron-process-emails/route.ts
+++ app/api/cron-process-emails/route.ts
@@ -1,7 +1,7 @@
 import { prisma } from "@/lib/prisma";
 import { sendEmail } from "@/lib/sendgrid";
 
-export default async function GET() {
+export const GET = async () => {
   try {
     const emails = await prisma.emailQueue.findMany({
       where: {
